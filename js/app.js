7bde92a1003794ccb2ca99fc54721015d6dfd92e
germund.hammer <gh@ghammer.dk>
2015-03-24 10:22:26 +0100

    Updated in response too review
    Most important changes are:
    replaced sleep() resulting in much bretter performance
    moved all enemy functions to its prototype
    enhanced enemy movement (moveAhead)
    various chnages to code style

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// FROGGER
+// Frogger is a simple arcade style game to be run in a browser.
+// It features a single screen consisting of three parts, a strip of water, a 3 lane street and a piece of grass.  
+// It is now the objective of the hero (here called the player) to cross the road to the water. 
+// The tricky part is the road where there may bugs. 
+//
+
 //constants
 var config = {
 	FROGGER:'FROGGER',
@@ -14,87 +21,111 @@ var config = {
 	GRASSROWN:5,
 	SCRXFACTOR:101,
 	SCRYFACTOR:83,
-	NOOFENEMIES:2
-}
+	NOOFENEMIES:4,
+	ENEMYDELAY:10
+};
 
-var player = new Player(2,5);
+var player = new Player(2,5); // Declare the player with start position
 
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-	this.no = 0;
+var Enemy = function(no) {
+	this.no = no;
 	this.x = -1;
 	this.y = 1;
+	this.delay = config.ENEMYDELAY;
+	this.remdelay = 0;
 	this.speed = speed();
 	this.state = '';
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-	//
-	// The following functions are for debugging purposes
-	this.setno = function(no) { //provide each enemy with a number
-		this.no = no;
-	}
-	this.setposition = function(x,y) { 
-		this.x = x;
-		this.y = y;
-	}
-	this.getx = function() {
-		return this.x;
-	}
-	this.gety = function() {
-		return this.y;
-	}
-	this.setx = function(x) {
-		this.x = x;
-	}
-	this.sety = function(y) {
-		this.y = y;
-	}
-	
-	this.setState = function(state) {
-		this.state = state;
-	}
-	this.getState = function() {
-		return this.state;
-	}
-	this.setspeed = function(speed) {
-		this.speed = speed;
-	}
-	this.reset = function() {
+};
+//
+// Enemy functions
+//
+Enemy.prototype.setNo = function(no) { //provide each enemy with a number
+	this.no = no;
+};
+Enemy.prototype.setPosition = function(x,y) { 
+	this.x = x;
+	this.y = y;
+};
+Enemy.prototype.getX = function() {
+	return this.x;
+};
+Enemy.prototype.getY = function() {
+	return this.y;
+};
+Enemy.prototype.setX = function(x) {
+	this.x = x;
+};
+Enemy.prototype.sety = function(y) {
+	this.y = y;
+};	
+Enemy.prototype.setState = function(state) {
+	this.state = state;
+};
+Enemy.prototype.getState = function() {
+	return this.state;
+};
+Enemy.prototype.setSpeed = function(speed) {
+	this.speed = speed;
+};
+Enemy.prototype.setLaneno = function() { // select a lane
+	var lane = Math.random()*3;
+	lane = lane - (lane % 1);
+	lane = lane + 1;
+	return lane;
+};
+function speed() { // select speed factor
+	return Math.random()*2;
+};
+
+Enemy.prototype.resetGame = function() {
 		this.x = -1;
 		this.y = Math.random()*3;	
 		this.state = '';
 		this.speed = speed();
-	}
-}
+};
+Enemy.prototype.checkFreespace = function(no, x, y) { // check for free space
+	allEnemies.forEach(function(enemy) {
+		if (x == (enemy.getX()) && y == enemy.getY() && no != enemy.no) { // place taken
+			return false;
+		};
+	});
+	return true;
+};
+Enemy.prototype.moveAhead = function() {
+	if (this.checkFreespace(this.no, this.x+1, this.y) == true) { this.x++; return; };
+	if (this.checkFreespace(this.no, this.x, this.y-1) == true) { this.y--; return; };
+	if (this.checkFreespace(this.no, this.x, this.y+1) == true) { this.y++; return; };
+};
 	
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-	
+	if (--this.remdelay >0) return; //delay slowing the enemy down so that the player stand a chance
+	this.remdelay=this.delay;
 	// Enemy state handling - this is where enemy behaviour is controlled
 	switch (this.state) {
 		case "":
 		case "waiting": // waiting for the player to appear on the road
-			if(player.gety() < 4) { // player is on the road
-				if (Math.random() > 0.3) { // start chasing (maybe)
+			if(player.getY() < 4) { // player is on the road
+				if (Math.random() > 1/config.NOOFENEMIES) { // start chasing (maybe)
 					this.state = "chasing"
 				};
 			};
 			break;
 		case "chasing": // chasing the player 
-			var diff = this.y - player.gety(); // close in on the player
-			if(diff < 0) this.y++;
-			if(diff > 0) this.y--; 
-			if(this.y>3) this.y = 3; // check that only the road is used
+			console.log('enemy# ' + this.no + ', state ' + this.state + ' x ' + this.x + ' y ' +  this.y);
+			var diff = this.y - player.getY(); // close in on the player
+			if(diff < 0) this.y++; // change to another lane
+			if(diff > 0) this.y--; // change to another lane 
+			if(this.y>3) this.y = 3; // check that the road is used
 			if(this.y<1) this.y = 1;
 			if(this.x>4) this.state = 'offscreen'; // check for off screen'ess
-			if (checkFreespace(this.x+1, this.y) == true) this.x++; // advance to the right
+			//if (this.checkFreespace(this.no, this.x+1, this.y) == true) this.x++; 
+			this.x = Math.round(this.x);
+			this.y = Math.round(this.y);
+			this.moveAhead(); // avoid collision with other enemies
 			break;			
 		case 'offscreen': // run off the screen 
 			console.log('enemy# ' + this.no + ', state ' + this.state + ' x ' + this.x + ' y ' +  this.y);
@@ -102,19 +133,17 @@ Enemy.prototype.update = function(dt) {
 			break;
 		case 'gothim': // got the player
 			console.log('enemy# ' + this.no + ', state ' + this.state + ' x ' + this.x + ' y ' +  this.y);
-			resetGame();
+			restartGame();
 			break;
-	}
+	};
 	if (this.x < 5) { // don't display enemies outside the view
 		ctx.drawImage(Resources.get(this.sprite), this.x*dt, this.y*dt); 
-		sleep(300/this.speed); // delay the enemy
-	}
-}
-
+	};
+};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x*101, this.y*83);
-}
+};
 
 // This is the player class
 // It requires an update(), render() and
@@ -125,46 +154,46 @@ function Player(x, y) {
 	this.x = x;
 	this.y = y;
 	this.state = "waiting";
-	this.getx = function() {
+	this.getX = function() {
 		return this.x;
-	}
-	this.gety = function() {
+	};
+	this.getY = function() {
 		return this.y;
-	}
-	this.setx = function(x) {
+	};
+	this.setX = function(x) {
 		this.x = x;
-	}
+	};
 	this.sety = function(y) {
 		this.y = y;
-	}
-	this.getstate = function() {
+	};
+	this.getState = function() {
 		return this.state;
-	}
-	this.setstate = function(state) {
+	};
+	this.setState = function(state) {
 		this.state = state;
-	}
+	};
 	this.update = function(dt) {
-		if (this.state == 'dead') resetGame();
-		if (this.state == 'winner') resetGame();
+		if (this.state == 'dead') restartGame();
+		if (this.state == 'winner') restartGame();
 		if (checkCollisions() == false) {
 			checkWater();
-		}
-		if (checkEndofgame() == true) resetGame();
+		};
+		if (checkEndofgame() == true) restartGame();
 		return true;
 	};
 	this.render = function() {
-		if (this.getstate() == 'dying') {
+		if (this.getState() == 'dying') {
 			ctx.drawImage(Resources.get('images/char-boy-inv.png'), this.x*101, this.y*83); 
 			console.log('player dying at x ' + this.x + ', y ' + this.y);
-			this.setstate('dead');
+			this.setState('dead');
 			return true;
-		}
-		if (this.getstate() == 'dead') {
+		};
+		if (this.getState() == 'dead') {
 			console.log('player now dead at x ' + this.x + ', y ' + this.y);
-			this.setstate('waiting');
-			resetGame();
+			this.setState('waiting');
+			restartGame();
 			return true;
-		} // still alive
+		}; // still alive
 		ctx.drawImage(Resources.get(this.sprite), this.x*101, this.y*83); 
 		return true;
 	};
@@ -186,49 +215,7 @@ function Player(x, y) {
 		console.log('player ' + key + ' x ' + this.x + ', y ' + this.y);
 		return true;
 	};
-}
-
-function sleep(time) { // poor man's sleep function (aka Unix)
-	time += new Date().getTime();
-	while (new Date() < time){}
-	return true;
-}
-
-function laneno() { // randomly select one of 3 lanes
-	var lane = Math.random()*3;
-	lane = lane - (lane % 1);
-	lane = lane + 1;
-	return lane;
-}
-
-function speed() { // randomly select on of 3 speed factors
-	return Math.random()*2; // was 3
-}
-
-function checkFreespace(x, y) { // checxk for free space to avoid enemies occupy the same spot
-	allEnemies.forEach(function(enemy) {
-		if (x == (enemy.getx()) && y == enemy.gety()) {
-				return false;
-		};
-	});
-	return true;
-}
-
-// The objects are instantiated
-// The enemy objects are placed in an arraysleep
-// We create a single instance
-var enemy = new Enemy();
-// Set up the array
-var allEnemies = [];
-var number_of_enemies = config.NOOFENEMIES;
-for (i = 0; i < number_of_enemies; i++) { 
-	allEnemies.push(clone(enemy)); // and clone the enemies we need
-	allEnemies[i].setno(i+1); // set the enemy number
-}
-// Place the player object in a variable called player
-var player = new Player(2,5);  // Player instance wit start position parameters
-
-alert('Start the game using the arrow keys');
+};
 
 // A listener is set up for key presses and sends the keys to the
 // Player.handleInput() method.
@@ -244,23 +231,21 @@ document.addEventListener('keyup', function(e) {
 });
 
 function checkEndofgame() { // are all the enemies off the screen?
-	console.log('check for lack of enemies at x ' + player.getx() + ', y ' + player.gety());	
 	var ctr = allEnemies.length;
 	allEnemies.forEach(function(enemy) {
 		if (enemy.getState() == 'offscreen') {
-//		if (enemy.getx() > 4) {
 			ctr--;
-		}
+		};
 	});
 	if (ctr == 0) { return true } else { return false };
-}
+};
 
 function checkCollisions() { // check that the player has collided with one of the enemies
 	var returnvalue = false;
 	allEnemies.forEach(function(enemy) {
-		if (player.getx() == enemy.getx() && player.gety() == enemy.gety()) {
-			player.setstate('dying'); // the dying / dead states are used to make the collision visible (allowing for an extra screen refresh)
-			console.log('collision: x ' + player.getx() + ', y ' + player.gety());
+		if (player.getX() == enemy.getX() && player.getY() == enemy.getY()) {
+			player.setState('dying'); // the dying / dead states are used to make the collision visible (allowing for an extra screen refresh)
+			console.log('collision: x ' + player.getX() + ', y ' + player.getY());
 			returnvalue = true;
 		};
 	});
@@ -268,44 +253,51 @@ function checkCollisions() { // check that the player has collided with one of t
 };
 
 function checkWater() { // check whether the player is in the water
-	if (player.gety() < 1) { // we are in the water
+	if (player.getY() < 1) { // we are in the water
 		//player.render();
-		console.log('the player reached the water: x ' + player.getx() + ', y ' + player.gety());
-		player.setstate('winner');
+		console.log('the player reached the water: x ' + player.getX() + ', y ' + player.getY());
+		player.setState('winner');
 		return true;
-	}
+	};
 	return false;
 };
 
-function resetGame() { // reset the game either because the player was caught or all the enemies are off the screen
-	switch (player.getstate()) {
+function restartGame() { // resetGame the game either because the player was caught or all the enemies are off the screen
+	switch (player.getState()) {
 			case "winner":
-				alert('Congratulation, wet but alive!!');
+				alert('Congratulation!!');
 				break;
 			case "dead":
-				alert('You are so history!!');
+				alert('You are out!!');
 				break;
 			default:
 				alert('Game over');
 				break;
 	}
 	allEnemies.forEach(function(enemy) {
-		enemy.reset();
+		enemy.resetGame();
 	});
-	player.setx(2); // reset player
+	player.setX(2); // resetGame player
 	player.sety(5);
-	player.setstate('waiting');
-};	
+	player.setState('waiting');
+};
 
-function clone(obj) { // clone code inspired by internet search
-  var copy = Object.create(Object.getPrototypeOf(obj));
-  var propNames = Object.getOwnPropertyNames(obj);
-  propNames.forEach(function(name) {
-    var desc = Object.getOwnPropertyDescriptor(obj, name);
-    Object.defineProperty(copy, name, desc);
-  });
-  return copy;
-}
+//
+// main body
+//
+// - objects are instantiated
+// - enemy objects are placed in an array
+var allEnemies = [];
+var number_of_enemies = config.NOOFENEMIES;
+for (i = 0; i < number_of_enemies; i++) { 
+	allEnemies.push(new Enemy(i+1)); // create new enemy with enemy number
+};
+// Place the player object in a variable called player
+var player = new Player(2,5);  // Player instance wit start position parameters
+
+alert('Start the game using the arrow keys');
+	
+
 
 
 
